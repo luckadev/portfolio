2a1bb0bc202749b3d73269fa0d7c0829e09ea7c3
Lucas Ribeiro <lucasribeiro.130605@gmail.com>
2023-03-03 12:08:13 -0300

    responsive layout

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 // custom scrollbar
 // custom pointer
 // animation infinite h1 gradient
+// favicon
+// tech description
+
+//scrollReaveal
+ScrollReveal().reveal('.cta', { delay: 400 });
+ScrollReveal().reveal('.sobre', { delay: 400 });
+ScrollReveal().reveal('.servicosBox', { delay: 400 });
+ScrollReveal().reveal('.projetosBox', { delay: 400 });
+ScrollReveal().reveal('.tech', { delay: 400 });
+ScrollReveal().reveal('.contatolinks', { delay: 400 });
